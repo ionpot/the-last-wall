91e90e763b97ee7fa091a8f67842a7a6b5e3a1b5
Emir Kotan <emirkotan@gmail.com>
2017-12-28 00:04:27 +0000

    max_nations = 3

--- game.ml
+++ game.ml
@@ -1,8 +1,13 @@
 type t =
   { deity : Deity.t;
+    max_nations : int;
     nations : Nation.t list;
     wall : Wall.t;
   }
 
 let make deity wall =
-  { deity; wall; nations = [] }
+  { deity;
+    max_nations = 3;
+    nations = []
+    wall;
+  }
