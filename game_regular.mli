cf663d5bc9eeb15b0176e8debe18a74485a1462b
Emir Kotan <emirkotan@gmail.com>
2018-05-18 17:31:23 +0100

    game_regular.mli

---
+++ game_regular.mli
@@ -0,0 +1,21 @@
+open Game_defs
+
+type party = Game_enemy.party
+type resource = Game_resource.t
+type support = Game_support.t
+
+type event =
+  | Attack of party list
+  | Blessing of resource
+  | Casualty of (manpower * leader)
+  | End
+  | Nations of nation list
+  | Scout of party list
+  | Starvation of resource
+  | Support of support list
+  | Turn of turn
+  | Upkeep of resource
+
+module type T = Phase with type event_def := event
+
+module Make(State : Game_state.T) : T
