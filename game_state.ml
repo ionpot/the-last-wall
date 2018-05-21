69ee4211b6f833d63fed26a6551f535ac95be079
Emir Kotan <emirkotan@gmail.com>
2018-05-21 01:04:39 +0100

    game_state manp/supp

--- game_state.ml
+++ game_state.ml
@@ -23,13 +23,19 @@ module type T = sig
   val get_res : unit -> resource
   val add_res : resource -> unit
   val sub_res : resource -> unit
+  val add_manp : manpower -> unit
+  val sub_manp : manpower -> unit
+  val no_manp : unit -> bool
+  val add_supp : supply -> unit
+  val sub_supp : supply -> unit
+  val clr_supp : unit -> unit
+  val missing_supp : unit -> supply
   val get_deity : unit -> deity
   val set_deity : deity -> unit
   val get_nats : unit -> nation list
   val set_nats : nation list -> unit
   val get_ldr : unit -> leader
   val set_ldr : leader -> unit
-  val defeat : unit -> bool
 end
 
 module Make( ) : T = struct
@@ -52,6 +58,17 @@ module Make( ) : T = struct
   let get_res () = t.res
   let add_res r = t.res <- t.res ++ r
   let sub_res r = t.res <- t.res -- r
+
+  let add_manp m = t.res <+ Manpwr m
+  let sub_manp m = t.res <~ Manpwr m
+  let no_manp () = manp t.res <= 0
+
+  let add_supp m = t.res <+ Supply m
+  let sub_supp m = t.res <~ Supply m
+  let clr_supp () = t.res <- make (Manpwr (manp t.res))
+  let missing_supp () =
+    let x = supp t.res in
+    if x < 0 then abs x else 0
 
   let get_deity () = t.deity
   let set_deity d = t.deity <- d
--- game_state.mli
+++ game_state.mli
@@ -8,13 +8,19 @@ module type T = sig
   val get_res : unit -> resource
   val add_res : resource -> unit
   val sub_res : resource -> unit
+  val add_manp : manpower -> unit
+  val sub_manp : manpower -> unit
+  val no_manp : unit -> bool
+  val add_supp : supply -> unit
+  val sub_supp : supply -> unit
+  val clr_supp : unit -> unit
+  val missing_supp : unit -> supply
   val get_deity : unit -> deity
   val set_deity : deity -> unit
   val get_nats : unit -> nation list
   val set_nats : nation list -> unit
   val get_ldr : unit -> leader
   val set_ldr : leader -> unit
-  val defeat : unit -> bool
 end
 
 module Make( ) : T
