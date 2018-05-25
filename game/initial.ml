0acbb83215649896b8cb63464ad4ded2fd2a8f50
Emir Kotan <emirkotan@gmail.com>
2018-05-25 11:51:02 +0100

    SendScouts >> initial

--- game/initial.ml
+++ game/initial.ml
@@ -2,6 +2,7 @@ type event =
   | Deity of Deity.t
   | End
   | Nations of Nation.t list
+  | SendScouts of bool
   | Starting of Resource.t
   | Support of Nation.support list
 
@@ -11,16 +12,18 @@ module Make(M : State.T) : T = struct
   let first () = Deity (M.get_deity ())
 
   let apply = function
-    | Starting x -> M.add_res x
-    | Support x -> M.add_res (Nation.total_of x)
     | Deity x -> M.set_deity x
     | End -> ()
     | Nations x -> M.set_nats x
+    | SendScouts x -> M.set_scouting x
+    | Starting x -> M.add_res x
+    | Support x -> M.add_res (Nation.total_of x)
 
   let next_of = function
     | Deity _ -> Starting (Deity.starting (M.get_deity ()))
     | Starting _ -> Nations (M.get_nats ())
-    | Nations _ -> Support (Nation.support_of_list (M.get_nats ()))
+    | Nations _ -> SendScouts (M.is_scouting ())
+    | SendScouts _ -> Support (Nation.support_of_list (M.get_nats ()))
     | Support _ -> End
     | End -> End
 
