de20a358e41b93a5aeacb3b58ada3a71f11ee5db
Emir Kotan <emirkotan@gmail.com>
2018-08-28 02:02:45 +0100

    resource set/deduce manp/supp

--- game/resource.ml
+++ game/resource.ml
@@ -15,10 +15,24 @@ let manp_of = fst
 let supp_of = snd
 let has_manp (x, _) = x > 0
 let has_supp (_, x) = x > 0
+let set_manp (_, x) y = (y, x)
+let set_supp (x, _) y = (x, y)
 let mis_supp (_, x) = of_supp ~-(min 0 x)
 let clr_supp (x, _) = (x, 0)
 let manp2supp (x, _) = (0, x)
 let supp2manp (_, x) = (x, 0)
+
+let deduce a b =
+  let d = a - b in
+  if d > 0 then d, 0 else 0, abs d
+
+let deduce_manp a b =
+  let x, y = deduce (manp_of a) (manp_of b) in
+  set_manp a x, set_manp b y
+
+let deduce_supp a b =
+  let x, y = deduce (supp_of a) (supp_of b) in
+  set_supp a x, set_supp b y
 
 let (<+) t = function
   | Empty -> t
--- game/resource.mli
+++ game/resource.mli
@@ -11,12 +11,17 @@ val empty : t
 val has_manp : t -> bool
 val of_manp : manpower -> t
 val manp_of : t -> manpower
+val set_manp : t -> manpower -> t
 
 val has_supp : t -> bool
 val of_supp : supply -> t
 val supp_of : t -> supply
+val set_supp : t -> supply -> t
 val mis_supp : t -> t
 val clr_supp : t -> t
+
+val deduce_manp : t -> t -> t * t
+val deduce_supp : t -> t -> t * t
 
 val manp2supp : t -> t
 val supp2manp : t -> t
