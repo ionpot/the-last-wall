8396e499424d1ec6037a49fda185d0db259c8fd2
Emir Kotan <emirkotan@gmail.com>
2018-11-24 00:26:15 +0000

    fix types in value.ml

--- util/value.ml
+++ util/value.ml
@@ -1,10 +1,10 @@
-module type Of = sig
+module type From = sig
   type t
   val empty : t
 end
 
-module type OfBit = Of with type t = bool
-module type OfNum = Of with type t = int
+module type FromBit = From with type t := bool
+module type FromNum = From with type t := int
 
 module type Val = sig
   type t
@@ -16,22 +16,22 @@ module type Val = sig
 end
 
 module type Bit = sig
-  include Val with type t = bool
+  include Val with type t := bool
   val clr : unit -> unit
   val flip : unit -> unit
   val set : unit -> unit
 end
 
 module type Num = sig
-  include Val with type t = int
-  val add : t -> unit
-  val sub : t -> unit
+  include Val with type t := int
+  val add : int -> unit
+  val sub : int -> unit
   val clr : unit -> unit
-  val deduce : t -> t
-  val deduce_from : t -> t
+  val deduce : int -> int
+  val deduce_from : int -> int
 end
 
-module Of (M : Of) = struct
+module From (M : From) = struct
   type t = M.t
   let x = ref M.empty
   let get () = !x
@@ -41,15 +41,25 @@ module Of (M : Of) = struct
   let map f = set (f !x)
 end
 
-module Bit (M : OfBit) = struct
-  include Of(M)
+module Of (M : From) : Val = struct
+  include From(M)
+end
+
+module Bit (M : FromBit) : Bit = struct
+  include From(struct
+    type t = bool
+    let empty = M.empty
+  end)
   let clr () = set false
   let flip () = map not
   let set () = set true
 end
 
-module Num (M : OfNum) = struct
-  include Of(M)
+module Num (M : FromNum) : Num = struct
+  include From(struct
+    type t = int
+    let empty = M.empty
+  end)
   let add i = map ((+) i)
   let sub i = map ((-) i)
   let clr () = set 0
@@ -61,24 +71,13 @@ module Num (M : OfNum) = struct
     set b; a
 end
 
-module BitSet = struct
-  type t = bool
-  let empty = true
-end
+module BitSet : FromBit = struct let empty = true end
+module BitClr : FromBit = struct let empty = false end
 
-module BitClr = struct
-  type t = bool
-  let empty = false
-end
-
-module Zero = struct
-  type t = int
-  let empty = 0
-end
+module Zero : FromNum = struct let empty = 0 end
 
 let of_num x =
-  let module M = struct
-    type t = int
+  let module M : FromNum = struct
     let empty = x
   end in
   (module Num(M) : Num)
