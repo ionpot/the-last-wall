a2e385c0c072e0925ea7f2a5c50162f4a23b909a
Emir Kotan <emirkotan@gmail.com>
2018-12-26 18:01:15 +0000

    smite boost -> 15-35 + 15

--- game/smite.ml
+++ game/smite.ml
@@ -1,11 +1,11 @@
 module Check (M : State.S) = struct
   let boost =
     if M.bld_ready Building.Temple
-    then 10
+    then 15
     else 0
 
   let find () =
-    let x = Dice.between 10 30 + boost in
+    let x = Dice.between 15 35 + boost in
     M.with_enemies Enemy.(find x Skeleton)
 
   let value =
