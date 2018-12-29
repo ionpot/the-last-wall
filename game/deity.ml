a246b96acb02f213d7f6324718e844c8b8f21175
Emir Kotan <emirkotan@gmail.com>
2018-12-29 16:48:11 +0000

    boosted blessing fix

--- game/deity.ml
+++ game/deity.ml
@@ -21,8 +21,10 @@ let blessing_of = function
 
 let boosted_of = function
   | Arnerula -> empty <+ rand 0 50
+  | Elanis as x -> blessing_of x <+ Manpwr 10
   | Lerota -> empty
-  | deity -> blessing_of deity <+ Manpwr 10 <+ Supply 10
+  | Sekrefir as x -> blessing_of x <+ Manpwr 10 <+ Supply 10
+  | Sitera as x -> blessing_of x <+ Supply 10
 
 let starting deity =
   blessing_of deity
