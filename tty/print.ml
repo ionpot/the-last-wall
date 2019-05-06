d64f4b7c14273f82b473d1ed1c6cf38c313b21ca
Emir Kotan <emirkotan@gmail.com>
2019-05-06 22:40:37 +0100

    remove commented-out code

--- tty/print.ml
+++ tty/print.ml
@@ -72,17 +72,3 @@ let support ls =
   |> List.map (fun (nat, res) -> (nation2str nat, f res))
   |> List.map (fun (nat, res) -> sprintf "%s sent %s" nat res)
   |> Tty.writelns
-
-  (*
-let defense men cav ldr =
-  let str = defense2str men cav ldr in
-  Tty.pairln "defense" str;
-  if Cavalry.too_many cav men
-  then Tty.writeln "too many cavalry, defense reduced"
-
-let queued ls =
-  List.map (Pair.map (bld2str, res2str)) ls
-  |> List.map (fun (b, r) -> sprintf "%s (%s)" b r)
-  |> commas
-  |> Tty.pairln "unfinished"
-  *)
