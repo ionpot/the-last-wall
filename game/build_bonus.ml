4c4848f14bb4f432d3a8febf70aca4da5585b733
Emir Kotan <emirkotan@gmail.com>
2019-06-08 13:50:13 +0100

    elanis stables 10% cheaper

--- game/build_bonus.ml
+++ game/build_bonus.ml
@@ -1,7 +1,9 @@
 module From (S : State.S) = struct
   let ls =
     [ S.Build.check Build.(ready Engrs),
-      Build.(ToAll, Resource.Bonus.(Sub (Sup 0.1)))
+      Build.(ToAll, Resource.Bonus.(Sub (Sup 0.1)));
+      S.Deity.is Deity.Elanis,
+      Build.(To Stable, Resource.Bonus.(Sub (Both 0.1)))
     ]
   let value = List.filter fst ls |> List.map snd
 end
