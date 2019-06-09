3bb22eb4c5f0a780f8f4f88fbab3ddf836b56fd4
Emir Kotan <emirkotan@gmail.com>
2019-06-09 22:55:14 +0100

    temple.promotable does not need cap check

--- game/temple.mli
+++ game/temple.mli
@@ -1,7 +1,7 @@
 module With : State.S -> sig
   val cap_rem : unit -> Defs.count
   val cap_for : Units.kind -> Defs.count
-  val promotable : Units.kind -> Defs.count
+  val promotable : unit -> Defs.count
 
   val buy : Units.kind -> Defs.count -> unit
   val promote : Units.kind -> Defs.count -> unit
