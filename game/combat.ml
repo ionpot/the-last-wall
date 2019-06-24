b78fadd33fbb3ddaddd04fb7abc9501887cc9199
Emir Kotan <emirkotan@gmail.com>
2019-06-24 22:15:35 +0100

    dr.ml

--- game/combat.ml
+++ game/combat.ml
@@ -27,34 +27,14 @@ module Apply (S : State.S) = struct
     end
 end
 
-let barrage_dr = 0.05
-let cav_unit_dr = 0.002
-let cav_dr_penalty = 0.05
-let cav_men_ratio = 0.4
 let fort_cap = 20.
-let harpy_dr = 0.002
-let mausoleum_dr = 0.01
 
-let floor_harpy_dr = Float.floor_by 0.01
 let to_power = Defs.to_power
-
-let cav_dr cav too_many snow =
-  if too_many then -.cav_dr_penalty
-  else if snow then 0.
-  else to_power cav cav_unit_dr
 
 module Units (S : State.S) = struct
   module Dist = Units.Dist(S.Dice)
   module Fill = Units.Fill(S.Dice)
   let attack = S.Enemy.return Units.power
-  let harpies = S.Enemy.return Units.(count Harpy)
-  let cavs = S.Units.return Units.(count Cavalry)
-  let snow = S.Weather.is Weather.(Snow Heavy)
-  let infantry = S.Units.return Units.count_infantry
-  let ratio = Number.ratio cavs infantry
-  let ratio_bonus = if S.Deity.is Deity.Elanis then 0.1 else 0.
-  let cav_too_many = ratio > cav_men_ratio +. ratio_bonus
-  let cav_dr = cav_dr cavs cav_too_many snow
   let units = S.Units.return Units.(rm Ballista)
   let power = Units.power units
   let fled () = Fill.from fort_cap units
@@ -64,27 +44,16 @@ module Units (S : State.S) = struct
 end
 
 module Make (S : State.S) = struct
-  module Roll = struct
-    module Leader = Leader.Roll(S.Dice)
-  end
+  module Dr = Dr.From(S)
+  module LdrRoll = Leader.Roll(S.Dice)
   module Units = Units(S)
-  let ldr_alive = S.Leader.check Leader.is_alive
-  let ldr_dr =
-    if ldr_alive then S.Leader.return Leader.defense_of else 0.
-  let barrage_dr =
-    if ldr_alive then S.Barraging.either barrage_dr 0. else 0.
-  let harpy_dr = floor_harpy_dr (to_power Units.harpies harpy_dr)
-  let harpy_weaken = to_power Units.harpies (S.Barraging.either 1. 0.)
-  let mausoleums = S.Build.return Build.mausoleums
-  let mausoleum_dr =
-    let bonus = if S.Deity.is Deity.Lerota then 2 else 1 in
-    to_power (mausoleums * bonus) mausoleum_dr
+
+  let harpy_weaken = to_power Dr.harpies (S.Barraging.either 1. 0.)
 
   let value = (module struct
-    let cav_too_many = Units.cav_too_many
+    let cav_too_many = Dr.cav_too_many
     let attack = Units.attack -. harpy_weaken
-    let defense =
-      Units.cav_dr +. ldr_dr +. mausoleum_dr -. barrage_dr -. harpy_dr
+    let defense = Dr.value
     let damage = Float.reduce attack defense
     let defeat = damage > Units.power
     let retreat = defeat && S.Build.check Build.(ready Fort)
@@ -93,6 +62,6 @@ module Make (S : State.S) = struct
     let enemies = Units.enemy_loss power
     let ldr_died =
       if retreat then false
-      else S.Leader.check Roll.Leader.death
+      else S.Leader.check LdrRoll.death
   end : Outcome)
 end
---
+++ game/dr.ml
@@ -0,0 +1,41 @@
+let barrage_dr = 0.05
+let cav_unit_dr = 0.002
+let cav_dr_penalty = 0.05
+let cav_men_ratio = 0.4
+let harpy_dr = 0.002
+let mausoleum_dr = 0.01
+
+let floor_harpy_dr = Float.floor_by 0.01
+let to_power = Defs.to_power
+
+let cav_dr cav too_many snow =
+  if too_many then -.cav_dr_penalty
+  else if snow then 0.
+  else to_power cav cav_unit_dr
+
+module From (S : State.S) = struct
+  let cavs = S.Units.return Units.(count Cavalry)
+  let snow = S.Weather.is Weather.(Snow Heavy)
+  let infantry = S.Units.return Units.count_infantry
+  let ratio = Number.ratio cavs infantry
+  let ratio_bonus = if S.Deity.is Deity.Elanis then 0.1 else 0.
+  let cav_too_many = ratio > cav_men_ratio +. ratio_bonus
+  let cav_dr = cav_dr cavs cav_too_many snow
+
+  let ldr_alive = S.Leader.check Leader.is_alive
+  let ldr_dr =
+    if ldr_alive then S.Leader.return Leader.defense_of else 0.
+  let barrage_dr =
+    if ldr_alive then S.Barraging.either barrage_dr 0. else 0.
+
+  let harpies = S.Enemy.return Units.(count Harpy)
+  let harpy_dr = floor_harpy_dr (to_power harpies harpy_dr)
+
+  let mausoleums = S.Build.return Build.mausoleums
+  let mausoleum_dr =
+    let bonus = if S.Deity.is Deity.Lerota then 2 else 1 in
+    to_power (mausoleums * bonus) mausoleum_dr
+
+  let value =
+      cav_dr +. ldr_dr +. mausoleum_dr -. barrage_dr -. harpy_dr
+end
