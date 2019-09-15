dc25d2a37023a6193614f57bf6d52ec4da97db12
Emir Kotan <emirkotan@gmail.com>
2019-09-15 22:39:31 +0100

    test_pick: print dist absorbed/healed/outcome

--- tty/test_pick.ml
+++ tty/test_pick.ml
@@ -3,7 +3,8 @@ open Printf
 
 module S = State.Make(Random)
 module A = Attack.Make(S)
-module Dist = Units.Dist(S.Dice)
+module Dist = Units.Dist
+module DistRoll = Dist.Roll(S.Dice)
 module Fill = Units.Fill(S.Dice)
 module FillC = Units.FillCount(S.Dice)
 
@@ -15,6 +16,9 @@ let units =
 
 let () =
   Random.self_init ();
-  Printf.printf "dist: %s\n" (Dist.from cap units |> Convert.units2str);
+  let r = DistRoll.from cap units in
+  Printf.printf "dist: %s\n" (Dist.outcome r |> Convert.units2str);
+  Printf.printf "absorbed: %s\n" (Dist.absorbed r |> Convert.power2str);
+  Printf.printf "healed: %s\n" (Dist.healed r |> Convert.power2str);
   Printf.printf "fill: %s\n" (Fill.from cap units |> Convert.units2str);
   Printf.printf "fillc: %s\n" (FillC.from cap' units |> Convert.units2str)
