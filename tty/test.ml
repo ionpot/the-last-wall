3b26a38811ffed49b4d183724c989bab51749991
Emir Kotan <emirkotan@gmail.com>
2019-09-24 09:10:05 +0100

    rm fills

--- tty/test.ml
+++ tty/test.ml
@@ -5,8 +5,6 @@ module S = State.Make(Random)
 module A = Attack.Make(S)
 module Dist = Units.Dist
 module DistRoll = Dist.Roll(S.Dice)
-module Fill = Units.Fill(S.Dice)
-module FillC = Units.FillCount(S.Dice)
 
 let cap = float_of_string Sys.argv.(1)
 let cap' = truncate cap
@@ -17,8 +15,6 @@ let units =
 let () =
   Random.self_init ();
   let r = DistRoll.from cap units in
-  Printf.printf "dist: %s\n" (Dist.outcome r |> Convert.units2str);
+  Printf.printf "casualty: %s\n" (Dist.outcome r |> Convert.units2str);
   Printf.printf "absorbed: %s\n" (Dist.absorbed r |> Convert.power2str);
-  Printf.printf "healed: %s\n" (Dist.healed r |> Convert.power2str);
-  Printf.printf "fill: %s\n" (Fill.from cap units |> Convert.units2str);
-  Printf.printf "fillc: %s\n" (FillC.from cap' units |> Convert.units2str)
+  Printf.printf "healed: %s\n" (Dist.healed r |> Convert.power2str)
