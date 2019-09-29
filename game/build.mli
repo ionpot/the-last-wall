f79662f569a16508063d0bcf9cf3ab3807a8490f
Emir Kotan <emirkotan@gmail.com>
2019-09-30 00:22:18 +0100

    refactor build

--- game/build.mli
+++ game/build.mli
@@ -3,41 +3,57 @@ open Defs
 type cost = Resource.t
 type kind = Arena | Engrs | Fort | Foundry | Guesthouse | Market | Mausoleum of Leader.t | Observatory | Sawmill | Stable | Tavern | Temple | Trade of Nation.trade
 type bonus = To of kind | ToAll
-type queued = kind * cost
-type status = kind list * kind list * queued list
 
 val starting : Leader.t -> kind
+
+module Avlb : sig
+  module Set : Set.S with type elt = kind
+  type t = Set.t
+end
 
 module Bonus : sig
   type t = bonus * Resource.Bonus.t
 end
+
+module Built : sig
+  type t = kind list
+end
+
+module Queue : sig
+  type t = (kind * cost) list
+end
+
+module Ready : sig
+  module Map : Map.S with type key = kind
+  type t = count Map.t
+end
+
+type status = Built.t * Built.t * Queue.t
 
 type t
 
 val empty : t
 
 val cost_of : kind -> Bonus.t list -> cost
+val is_multiple : kind -> bool
 val manpwr_range : kind -> manpower range
 val supply_range : kind -> supply range
-val multiple : kind -> bool
 
 val arena_cap : t -> count
+val available : t -> Avlb.t
 val ballista_cap : t -> count
-val built : kind -> t -> bool
 val count : kind -> t -> count
-val ls_avlb : t -> kind list
-val ls_built : t -> kind list
-val ls_queue : t -> queued list
-val ls_ready : t -> kind list
+val is_built : kind -> t -> bool
+val is_ready : kind -> t -> bool
 val mausoleums : t -> count
 val need_manp : t -> manpower
 val need_supp : t -> supply
-val ready : kind -> t -> bool
+val need_trade : t -> bool
+val ready : t -> Ready.t
 val stable_cap : t -> count
 val status : t -> status
 val temple_cap : t -> count
-val trade : t -> Nation.trade
-val trade_not_set : t -> bool
+val trade_type : t -> Nation.trade
 
 val died : Leader.t -> t -> t
 val manp : manpower -> manpower -> t -> t
