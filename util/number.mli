017152b584b2d1e4fce795e56d8b5b2a1db54e39
Emir Kotan <emirkotan@gmail.com>
2019-11-19 08:10:53 +0000

    number.mli

---
+++ util/number.mli
@@ -0,0 +1,17 @@
+val add_if : bool -> int -> int -> int
+val add_if_ptv : int -> int -> int
+val deduce : int -> int -> int * int
+val div : int -> int -> int
+val if_ok : int -> bool -> int
+val increase_by : float -> int -> int
+val maybe : int -> int option -> int
+val opt_min : int option -> int -> int
+val opt2_min : int option -> int option -> int
+val portion : float -> int -> int
+val ratio : int -> int -> float
+val reduce_by : float -> int -> int
+val sub : int -> int -> int
+val sub_by : int -> int -> int
+val sub_if : bool -> int -> int -> int
+val sub_opt : int -> int -> int option
+val take : int -> int -> int * int
